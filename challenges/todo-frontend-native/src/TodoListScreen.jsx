48bc24b3454964f2401dbab78b3d02261813d653
t.r.d.vangraft@student.tudelft.nl <t.r.d.vangraft@student.tudelft.nl>
2019-12-29 20:46:06 +0100

    start working on intergrating the api

--- challenges/todo-frontend-native/src/TodoListScreen.jsx
+++ challenges/todo-frontend-native/src/TodoListScreen.jsx
@@ -1,19 +1,44 @@
-import React, { Component } from 'react';
-import { Button } from 'react-native';
+import React, { Component, useState, useEffect } from 'react';
+import { Button, View } from 'react-native';
+import { ListItem } from 'react-native-material-ui';
 
-class TodoListScreen extends Component {
-    static navigationOptions = {
-        title: "Welcome"
-    }
+const TodoListScreen = props => {
+    const { navigate } = props.navigation
+    const [todos, setTodos] = useState([])
 
-    render() {
-        const { navigate } = this.props.navigation
-        return (
-            <Button
-                title="Go to Todo's"
-                onPress={() => navigate('Todo', { todo: 'Test' })}
-            />
-        )
+    /**
+     * The component did mount function, load the todos from the api
+     */
+    useEffect(() => {
+        fetch(`http://127.0.0.1:5000/v1/todo`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(res => res.json())
+            .then(response => setTodos(response))
+            .catch(err => console.log(err))
+    }, [])
+        
+    return (
+        <View>
+            <Button title="This is the todo go back" />
+            {todos.map(todo => {
+                <ListItem
+                    divider
+                    centerElement={{
+                        primaryText: todo.title
+                    }}
+                />
+            })}
+        </View>
+    )
+}
+
+TodoListScreen.navigationOptions = () => {
+    return {
+        title: 'Welcome todo list'
     }
 }
 
